2d5d3eaa00caddb119ec75e2b1042816b1fd7465
cyy <cyyever@outlook.com>
2022-02-23 14:11:09 +0800

    add code

--- fnn_model.py
+++ fnn_model.py
@@ -4,14 +4,26 @@ import torch.nn as nn
 class FNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, token_num, seq_len, hidden_dim=None, use_direct_connection=True):
+    def __init__(
+        self,
+        token_num,
+        seq_len,
+        hidden_dim=None,
+        embedding_dim=None,
+        use_direct_connection=True,
+    ):
         super().__init__()
-        embedding_dim = 300
         self.token_num = token_num
+        self.seq_len = seq_len
+        if embedding_dim is None:
+            embedding_dim = 30
         self.embedding = nn.Embedding(token_num, embedding_dim=embedding_dim)
         self.use_direct_connection = use_direct_connection
         if self.use_direct_connection:
             self.direct_connection_fc = nn.Linear(seq_len * embedding_dim, token_num)
+        else:
+            print("no direct_connection_fc")
+
         if hidden_dim is None:
             hidden_dim = 100
         self.fc2 = nn.Linear(seq_len * embedding_dim, hidden_dim)
