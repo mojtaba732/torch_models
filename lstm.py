3af9f9b4e25b5b6f56fb24b25bbacc47ddb698c7
cyy <cyyever@outlook.com>
2022-04-10 21:28:43 +0800

    add code

--- lstm.py
+++ lstm.py
@@ -30,21 +30,6 @@ class SimpleLSTM(nn.Module):
 
     def forward(self, text):
         embedded = self.dropout(self.embedding(text))
-        # pack sequence
-        # lengths need to be on CPU!
-        packed_embedded = embedded
-        # packed_embedded = nn.utils.rnn.pack_padded_sequence(
-        #     embedded, text_lengths.to("cpu")
-        # )
-        packed_output, (hidden, cell) = self.rnn(packed_embedded)
-        # unpack sequence
-        # output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
-        # output = [sent len, batch size, hid dim * num directions]
-        # output over padding tokens are zero tensors
-        # hidden = [num layers * num directions, batch size, hid dim]
-        # cell = [num layers * num directions, batch size, hid dim]
-        # concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
-        # and apply dropout
+        _, (hidden, _) = self.rnn(embedded)
         hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
-        # hidden = [batch size, hid dim * num directions]
         return self.fc(hidden)
