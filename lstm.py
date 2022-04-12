2e8bd2daec9fbcd8bed38dc7f4025064e09f6c55
cyy <cyyever@outlook.com>
2022-04-12 10:30:35 +0800

    fix code

--- lstm.py
+++ lstm.py
@@ -12,7 +12,6 @@ class SimpleLSTM(nn.Module):
         bidirectional=True,
         embedding_dim=250,
         hidden_dim=256,
-        dropout=0.5,
     ):
         super().__init__()
         self.embedding = nn.Embedding(
@@ -23,10 +22,9 @@ class SimpleLSTM(nn.Module):
             hidden_dim,
             num_layers=n_layers,
             bidirectional=bidirectional,
-            dropout=dropout,
         )
         self.fc = nn.Linear(hidden_dim * 2, num_classes)
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout()
 
     def forward(self, text):
         embedded = self.dropout(self.embedding(text))
